e8f5781dc72b9257cc30a8ef9638a928107f6334
Victor Dushi <victor.dushi@yahoo.fr>
2018-08-24 11:39:41 +0200

    Reafctoring

---
+++ TUI.AssessmentQuestion2/TUI.AssessmentQuestion2/Message.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TUI.AssessmentQuestion2
+{
+    abstract class Message
+    {
+        public abstract void MethodMessage();
+    }
+    class MessageA : Message
+    {
+        public override void MethodMessage()
+        {
+            // Implementing My Custom MethodMessageOnA 
+        }
+    }
+    class MessageB : Message
+    {
+        public override void MethodMessage()
+        {
+            // Implementing My Custom MethodMessageOnA 
+        }
+        public void MyOtherMethodOnB()
+        {
+            // Implementing My Other MethodMessageOnB
+        }
+    }
+    class MessageC : Message
+    {
+        public override void MethodMessage()
+        {
+            // Implementing My Custom MethodMessageOnC
+        }
+    }
+}
